f2494e14aabb5e78f79d319c4fb1768de1b4a5aa
usman <uumer58@yahoo.com>
2024-07-16 15:06:43 +0500

    Initial commit

---
+++ src/app/page.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// Import the colleges component if it's in a separate file
+import Colleges from '../components/colleges'; // Adjust this path if needed
+import Activities from '../components/activities';
+export default function Home() {
+    // const [showActivities, setShowActivities] = useState(false);
+  return (
+
+      <main>
+        {/*<Colleges />*/}
+          <Activities/>
+
+      </main>
+
+  );
+}
